82dca724b987cd501186088189c8712d3e4388b2
verytuna <tiagofortuna412@gmail.com>
2023-04-24 14:17:12 +0100

    add values dinamically

--- lists_of_data/lib/main.dart
+++ lists_of_data/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,10 +15,10 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'Be a monkey, not a banana',
-    'Quem não chora não mama',
-    'Sei la ou cralhes'
+  List<Quote> quotes = [
+    Quote(author: 'Oscar', text: ' macaco azul a voar'),
+    Quote(author: 'Quim', text: ' cavalos voadores'),
+    Quote(author: 'Cenouras', text: 'eu sei la')
   ];
 
   @override
@@ -30,7 +31,9 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.red,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes
+            .map((quote) => Text('${quote.text} - ${quote.author}'))
+            .toList(),
       ),
     );
   }
